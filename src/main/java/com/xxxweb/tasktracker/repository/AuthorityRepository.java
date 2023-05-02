adfdc3f593f72b63f0ae6cb2ed5845da70ee8a97
Ursu Rubin <ursurubin@MacBook-Pro-Ursu.local>
2023-05-02 22:10:24 +0300

    Initial version of tasktracker generated by generator-jhipster@7.9.3

---
+++ src/main/java/com/xxxweb/tasktracker/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.xxxweb.tasktracker.repository;
+
+import com.xxxweb.tasktracker.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
---
+++ src/main/java/com/xxxweb/tasktracker/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.xxxweb.tasktracker.service.dto;
